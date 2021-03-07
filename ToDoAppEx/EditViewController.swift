dcdb36a87958bd54d99bdfb9885aba2b3d4be456
izumiyoshiki <izumiyoshiki@ainoiMac.local>
2021-03-07 09:04:51 +0900

    add viewcontrollers

--- ToDoAppEx/EditViewController.swift
+++ ToDoAppEx/EditViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EditViewController.swift
 //  ToDoAppEx
 //
 //  Created by izumiyoshiki on 2021/03/07.
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegate {
-
+class EditViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegate {
+    
     @IBOutlet weak var navigationbar: UINavigationBar!
     @IBOutlet weak var tabbar: UITabBar!
     
@@ -16,13 +16,11 @@ class ViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegat
         let shareVC = storyboard?.instantiateViewController(withIdentifier: "ShareViewController") as! ShareViewController
         shareVC.modalPresentationStyle = .fullScreen
         self.present(shareVC, animated: true, completion: nil)
-
     }
     @IBAction func searchButton(_ sender: Any) {
         let searchVC = storyboard?.instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
         searchVC.modalPresentationStyle = .fullScreen
         self.present(searchVC, animated: true, completion: nil)
-
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,16 +35,17 @@ class ViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegat
     }
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        if(item.tag == 1) {
-            let editVC = storyboard?.instantiateViewController(withIdentifier: "EditViewController") as! EditViewController
-            editVC.modalPresentationStyle = .fullScreen
-            self.present(editVC, animated: true, completion: nil)
+        if(item.tag == 0) {
+            let homeVC = storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+            homeVC.modalPresentationStyle = .fullScreen
+            self.present(homeVC, animated: true, completion: nil)
         } else if(item.tag == 2) {
             let settingVC = storyboard?.instantiateViewController(withIdentifier: "SettingViewController") as! SettingViewController
             settingVC.modalPresentationStyle = .fullScreen
             self.present(settingVC, animated: true, completion: nil)
         }
     }
+
 
 }
 
--- ToDoAppEx/SettingViewController.swift
+++ ToDoAppEx/SettingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingViewController.swift
 //  ToDoAppEx
 //
 //  Created by izumiyoshiki on 2021/03/07.
@@ -7,22 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegate {
+class SettingViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegate {
 
     @IBOutlet weak var navigationbar: UINavigationBar!
     @IBOutlet weak var tabbar: UITabBar!
-    
     @IBAction func shareButton(_ sender: Any) {
         let shareVC = storyboard?.instantiateViewController(withIdentifier: "ShareViewController") as! ShareViewController
         shareVC.modalPresentationStyle = .fullScreen
         self.present(shareVC, animated: true, completion: nil)
-
     }
     @IBAction func searchButton(_ sender: Any) {
         let searchVC = storyboard?.instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
         searchVC.modalPresentationStyle = .fullScreen
         self.present(searchVC, animated: true, completion: nil)
-
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,16 +34,18 @@ class ViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegat
     }
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        if(item.tag == 1) {
+
+        if(item.tag == 0) {
+            let homeVC = storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+            homeVC.modalPresentationStyle = .fullScreen
+            self.present(homeVC, animated: true, completion: nil)
+        } else if(item.tag == 1) {
             let editVC = storyboard?.instantiateViewController(withIdentifier: "EditViewController") as! EditViewController
             editVC.modalPresentationStyle = .fullScreen
             self.present(editVC, animated: true, completion: nil)
-        } else if(item.tag == 2) {
-            let settingVC = storyboard?.instantiateViewController(withIdentifier: "SettingViewController") as! SettingViewController
-            settingVC.modalPresentationStyle = .fullScreen
-            self.present(settingVC, animated: true, completion: nil)
         }
     }
+
 
 }
 

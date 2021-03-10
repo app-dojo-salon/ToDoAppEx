38f1ea000067af2e496417c54f2a792c4709b8d2
Nao-RandD <nao.r.and.d@gmail.com>
2021-03-10 17:10:48 +0900

    add To-Do creation process to the edit screen.

--- ToDoAppEx/EditViewController.swift
+++ ToDoAppEx/EditViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import RealmSwift
 
 class EditViewController: UIViewController, UINavigationBarDelegate, UITabBarDelegate {
+	private var realm: Realm!
+	private var todoList: Results<TodoItem>!
+	private var token: NotificationToken!
     
-    @IBOutlet weak var navigationbar: UINavigationBar!
-    @IBOutlet weak var tabbar: UITabBar!
-    
+    @IBOutlet private weak var navigationbar: UINavigationBar!
+    @IBOutlet private weak var tabbar: UITabBar!
+	@IBOutlet private weak var tableView: UITableView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		navigationbar.delegate = self
+		tabbar.delegate = self
+	}
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+
+		realm = try! Realm()
+		todoList = realm.objects(TodoItem.self)
+		token = todoList.observe { [weak self] _ in
+		  self?.reload()
+		}
+	}
+
     @IBAction func shareButton(_ sender: Any) {
         let shareVC = storyboard?.instantiateViewController(withIdentifier: "ShareViewController") as! ShareViewController
         shareVC.modalPresentationStyle = .fullScreen
@@ -24,13 +46,35 @@ class EditViewController: UIViewController, UINavigationBarDelegate, UITabBarDel
         self.present(searchVC, animated: true, completion: nil)
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBAction private func tappedAddButton(_ sender: Any) {
+		let dlg = UIAlertController(title: "新規Todo", message: "",
+									preferredStyle: .alert)
+		dlg.addTextField(configurationHandler: nil)
+		dlg.addAction(UIAlertAction(title: "OK", style: .default,
+									handler: { action in
+		  if let t = dlg.textFields![0].text,
+			!t.isEmpty {
+			self.addTodoItem(title: t)
+		  }
+		}))
+		present(dlg, animated: true)
+	}
 
-        navigationbar.delegate = self
-        tabbar.delegate = self
-    }
+	private func addTodoItem(title: String) {
+		try! realm.write {
+		  realm.add(TodoItem(value: ["title": title]))
+		}
+	}
 
+	private func deleteTodoItem(at index: Int) {
+		try! realm.write {
+		  realm.delete(todoList[index])
+		}
+	}
+
+	private func reload() {
+		tableView.reloadData()
+	}
 	
     func position(for bar: UIBarPositioning) -> UIBarPosition {
         return .topAttached
@@ -48,6 +92,33 @@ class EditViewController: UIViewController, UINavigationBarDelegate, UITabBarDel
         }
     }
 
+	deinit {
+		token.invalidate()
+	}
+}
 
+extension EditViewController: UITableViewDataSource, UITableViewDelegate {
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return todoList.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "todoItem", for: indexPath)
+		cell.textLabel?.text = todoList[indexPath.row].title
+		return cell
+	}
+
+	func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+		return true
+	}
+
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+				   forRowAt indexPath: IndexPath) {
+		deleteTodoItem(at: indexPath.row)
+	}
 }
 
---
+++ ToDoAppEx/Model/TodoItem.swift
@@ -0,0 +1,13 @@
+//
+//  TodoItem.swift
+//  ToDoAppEx
+//
+//  Created by Naoyuki Kan on 2021/03/10.
+//
+
+import Foundation
+import RealmSwift
+
+class TodoItem: Object {
+  @objc dynamic var title = ""
+}

ae432fa22136884ad9e1b69590561e798d13d053
minmin0530 <izumiyoshiki@icloud.com>
2021-04-30 14:45:06 +0900

    correction login

--- ToDoAppEx/SignupViewController.swift
+++ ToDoAppEx/SignupViewController.swift
@@ -12,7 +12,7 @@ class SignupViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func createAccount(_ sender: Any) {
+    @IBAction private func createAccount(_ sender: Any) {
         let urlString = "http://tk2-235-27465.vs.sakura.ne.jp/insert_account"
         
         let request = NSMutableURLRequest(url: URL(string: urlString)!)
@@ -21,48 +21,36 @@ class SignupViewController: UIViewController {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let params:[String:Any] = [
-            "accountname": self.email.text,
-//            "chain": array3[0].chain,
-//            "point": array3[0].point,
-//            "time": array3[0].time//,
-         //   "date": rank.date
+            "accountname": self.email.text
         ]
-//        if monthFlag {
-//            params = [
-//                "name": self.rankingName[0].name,
-//                "chain": montharray3[0].chain,
-//                "point": montharray3[0].point,
-//                "time": montharray3[0].time//,
-//            ]
-//        }
-//
-//        if dayFlag {
-//            params = [
-//                "name": self.rankingName[0].name,
-//                "chain": dayarray3[0].chain,
-//                "point": dayarray3[0].point,
-//                "time": dayarray3[0].time//,
-//            ]
-//        }
+        
+        var errorFlag = false
         do{
             request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
 
             let task:URLSessionDataTask = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {(data,response,error) -> Void in
-                
-                
-                
-                //                        let resultData = String(data: data!, encoding: .utf8)!
-                //                        print("result:\(resultData)")
-                //                        print("response:\(response)")
+                if error != nil {
+                    DispatchQueue.main.sync {
+                        errorFlag = true
+                        self.email.text = "server error!!"
+                        print(error.debugDescription)
+                    }
+                    return
+                }
             })
             task.resume()
         }catch{
+            email.text = "server error!!"
             print("Error:\(error)")
             return
         }
 
-        let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
-        self.present(secondViewController, animated: true, completion: nil)
+        if errorFlag == false {
+            let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+            secondViewController.modalPresentationStyle = .fullScreen
+            self.present(secondViewController, animated: true, completion: nil)
+        }
+        return
 
     }
 }

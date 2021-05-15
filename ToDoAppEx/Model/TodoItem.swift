edd75cd1d2a432b47c4552315cb635c735046a7f
Nao-RandD <nao.r.and.d@gmail.com>
2021-05-16 08:58:45 +0900

    Fix AutoLayout for TableViewCell.

--- ToDoAppEx/Model/TodoItem.swift
+++ ToDoAppEx/Model/TodoItem.swift
@@ -11,4 +11,7 @@ import RealmSwift
 class TodoItem: Object {
 	@objc dynamic var title = ""
 	@objc dynamic var image = ""
+    @objc dynamic var category = ""
+    @objc dynamic var startDate = ""
+    @objc dynamic var endDate = ""
 }

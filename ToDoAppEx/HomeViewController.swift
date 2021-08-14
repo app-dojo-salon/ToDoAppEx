cddcd93add76d7ac9d631d420cc4bb0c9b5dc398
Nao-RandD <nao.r.and.d@gmail.com>
2021-08-14 13:07:49 +0900

    add edit function for category and date.

---
+++ ToDoAppEx/HomeViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  ToDoAppEx
+//
+//  Created by izumiyoshiki on 2021/03/07.
+//
+
+import UIKit
+import RealmSwift
+
+class HomeViewController: UIViewController {
+
+	private var todoList: Results<TodoItem>!
+	private var realm: Realm!
+	private var token: NotificationToken?
+
+	@IBOutlet private weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		print(#function)
+
+		tableView.dataSource = self
+		realm = try! Realm()
+		todoList = realm.objects(TodoItem.self)
+		token = todoList.observe { [weak self] _ in
+		  self?.reload()
+		}
+		tableView.reloadData()
+    }
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		print(#function)
+
+		reload()
+	}
+
+	private func deleteTodoItem(at index: Int) {
+		try! realm.write {
+		  realm.delete(todoList[index])
+		}
+	}
+
+	private func reload() {
+		tableView.reloadData()
+	}
+
+	deinit {
+        if let token = self.token {
+            token.invalidate()
+        }
+	}
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate{
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return todoList.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "todoItem", for: indexPath) as! ImageCell
+		// カスタムセルにRealmの情報を反映
+		cell.configure(image: todoList[indexPath.row].image,
+					   title: todoList[indexPath.row].title,
+                       category: todoList[indexPath.row].category,
+                       startDate: todoList[indexPath.row].startdate,
+                       endDate: todoList[indexPath.row].enddate)
+		return cell
+	}
+
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+				   forRowAt indexPath: IndexPath) {
+		deleteTodoItem(at: indexPath.row)
+	}
+}
--- ToDoAppEx/ImageCell.swift
+++ ToDoAppEx/ImageCell.swift
@@ -11,9 +11,16 @@ final class ImageCell: UITableViewCell {
 
 	@IBOutlet private weak var titleImageView: UIImageView!
 	@IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var categoryLabel: UILabel!
+    @IBOutlet private weak var startDateLabel: UILabel!
+    @IBOutlet private weak var endDateLabel: UIStackView!
 
-	func configure(image: String, title: String) {
+    func configure(image: String, title: String, category: String,
+                   startDate: String, endDate: String) {
 		titleImageView.image = UIImage(named: image)!
 		titleLabel.text = title
+        categoryLabel.text = category
+        print(startDate)
+        print(endDate)
 	}
 }

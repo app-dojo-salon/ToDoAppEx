b3bf378aa8716794e14a2daed17130fd0f03e592
Nao-RandD <nao.r.and.d@gmail.com>
2022-08-15 15:31:03 +0700

    [add] 検索画面のリストと検索窓の追加

--- ToDoAppEx/ViewController/SearchViewController.swift
+++ ToDoAppEx/ViewController/SearchViewController.swift
@@ -15,22 +15,6 @@ class SearchViewController: UIViewController {
         shareButton.tintColor = UIColor.clear
     }
 
-//    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-//        if(item.tag == 0) {
-//            let homeVC = storyboard?.instantiateViewController(withIdentifier: "ViewController") as! ViewController
-//            homeVC.modalPresentationStyle = .fullScreen
-//            self.present(homeVC, animated: true, completion: nil)
-//        } else if(item.tag == 1) {
-//            let editVC = storyboard?.instantiateViewController(withIdentifier: "EditViewController") as! EditViewController
-//            editVC.modalPresentationStyle = .fullScreen
-//            self.present(editVC, animated: true, completion: nil)
-//        } else if(item.tag == 2) {
-//            let settingVC = storyboard?.instantiateViewController(withIdentifier: "SettingViewController") as! SettingViewController
-//            settingVC.modalPresentationStyle = .fullScreen
-//            self.present(settingVC, animated: true, completion: nil)
-//        }
-//    }
-//
 
 }
 

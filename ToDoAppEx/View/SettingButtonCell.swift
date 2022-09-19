37fd0de1c36f35e0028c7c496ccb05d579457528
minmin0530 <izumiyoshiki@icloud.com>
2022-09-19 14:02:26 +0900

    fix setting screen

--- ToDoAppEx/View/SettingButtonCell.swift
+++ ToDoAppEx/View/SettingButtonCell.swift
@@ -11,8 +11,21 @@ class SettingButtonCell: UITableViewCell {
 
     @IBOutlet weak var settingButton: UIButton!
     
-    func configure(buttonName: String) {
+    var systemName: SettingViewController.SystemName?
+    var userInfoEdit: (() -> Void)?
+    var logout: (() -> Void)?
+    func configure(buttonName: String, systemName: SettingViewController.SystemName, userInfoEdit: @escaping () -> Void, logout: @escaping () -> Void ) {
         settingButton.setTitle(buttonName, for: .normal)
+        self.systemName = systemName
+        self.userInfoEdit = userInfoEdit
+        self.logout = logout
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        if systemName == SettingViewController.SystemName.userInfoEditButton {
+            self.userInfoEdit!()
+        } else if systemName == SettingViewController.SystemName.logoutButton {
+            self.logout!()
+        }
+    }
 }
--- ToDoAppEx/ViewController/SettingViewController.swift
+++ ToDoAppEx/ViewController/SettingViewController.swift
@@ -51,7 +51,10 @@ class SettingViewController: UIViewController {
         SystemName.logout: "ログイン状態",
         SystemName.logoutButton: ""
     ]
-    
+    private let userDefaults = UserDefaults()
+    private var email: UITextField?
+    private var accountName: UITextField?
+    private var password: UITextField?
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,11 +86,24 @@ extension SettingViewController: UITableViewDataSource, UITableViewDelegate {
             return cell
         } else if indexPath.row == SystemName.userInfoEditButton.rawValue || indexPath.row == SystemName.logoutButton.rawValue {
             let cell = tableView.dequeueReusableCell(withIdentifier: "SettingButtonCell", for: indexPath) as! SettingButtonCell
-            cell.configure(buttonName: systemNameArray[SystemName(rawValue: indexPath.row)!]!)
+            if indexPath.row == SystemName.userInfoEditButton.rawValue {
+                cell.configure(buttonName: systemNameArray[SystemName(rawValue: indexPath.row)!]!, systemName: SystemName.userInfoEditButton, userInfoEdit: self.editUserInfo, logout: logout)
+            } else if indexPath.row == SystemName.logoutButton.rawValue {
+                cell.configure(buttonName: systemNameArray[SystemName(rawValue: indexPath.row)!]!, systemName: SystemName.logoutButton, userInfoEdit: self.editUserInfo, logout: logout)
+            }
             return cell
         } else if indexPath.row == SystemName.accountName.rawValue || indexPath.row == SystemName.email.rawValue || indexPath.row == SystemName.password.rawValue {
             let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath) as! SettingCell
-            cell.configure(systemName: systemNameArray[SystemName(rawValue: indexPath.row)!]!, contentName: contentNameArray[SystemName(rawValue: indexPath.row)!]!)
+            cell.configure(systemName: systemNameArray[SystemName(rawValue: indexPath.row)!]!, contentName: contentNameArray[SystemName(rawValue: indexPath.row)!]!)            
+            if indexPath.row == SystemName.email.rawValue {
+                email = cell.contentNameTextField
+            }
+            if indexPath.row == SystemName.accountName.rawValue {
+                accountName = cell.contentNameTextField
+            }
+            if indexPath.row == SystemName.password.rawValue {
+                password = cell.contentNameTextField
+            }
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "SettingLabelCell", for: indexPath) as! SettingLabelCell
@@ -98,5 +114,41 @@ extension SettingViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         return
+    }
+    
+    func editUserInfo() {
+        let realm = try! Realm()
+        let allContents: Results<User> = realm.objects(User.self)
+        let serverRequest: ServerRequest = ServerRequest()
+        serverRequest.sendServerRequest(
+            urlString: "http://tk2-235-27465.vs.sakura.ne.jp/update_account",
+            params: [
+                "userid": allContents[0].userid,
+                "password": password?.text,
+                "email": email?.text,
+                "accountname": accountName?.text,
+                "publicprivate": allContents[0].publicprivate,
+                "sharepassword": allContents[0].sharepassword
+            ],
+            completion: self.dismissScreen(data:))
+    }
+    
+    func dismissScreen(data: Data?) {
+        DispatchQueue.main.async {
+            let list = try! Realm().objects(User.self)
+            try! Realm().write {
+                list[0].password = self.password!.text!
+                list[0].email = self.email!.text!
+                list[0].accountname = self.accountName!.text!
+            }
+            self.dismiss(animated: true)
+        }
+    }
+
+    func logout() {
+        DispatchQueue.main.async {
+            self.userDefaults.set(false, forKey: "login")
+            self.dismiss(animated: true)
+        }
     }
 }

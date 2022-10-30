0aff44823960a10e035a9f18bafa500a4f9a8e98
Nao-RandD <nao.r.and.d@gmail.com>
2022-10-30 13:35:35 +0900

    [add] SignUpViewModelを実装として追加

--- ToDoAppEx/ViewModel/SignUpViewModel.swift
+++ ToDoAppEx/ViewModel/SignUpViewModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import UIKit
+
+final class SignUpViewModel {
+    let changeText = Notification.Name("changeText")
+    let changeColor = Notification.Name("changeColor")
+    let activateButton = Notification.Name("activateButton")
+
+    private let notificationCenter: NotificationCenter
+    private let model: SignUpModelProtocol
+
+    init(notificationCenter: NotificationCenter, model: SignUpModelProtocol = SignUpModel()) {
+        self.notificationCenter = notificationCenter
+        self.model = model
+    }
+
+    func textFieldChanged(email: String?, password: String?,
+                          confirmPassword: String?, displayName: String?) {
+        let result = model.validate(email: email, password: password, confirmPassword: confirmPassword, displayName: displayName)
+
+        switch result {
+        case .success:
+            notificationCenter.post(name: changeText, object: "OK!!!")
+            notificationCenter.post(name: changeColor, object: UIColor.green)
+            notificationCenter.post(name: activateButton, object: true)
+        case .failure(let error as SignUpModelError):
+            notificationCenter.post(name: changeText, object: error.errorText)
+            notificationCenter.post(name: changeColor, object: UIColor.red)
+            notificationCenter.post(name: activateButton, object: false)
+        case _:
+            fatalError("Unexpected pattern.")
+        }
+    }
+}
+
+extension SignUpModelError {
+    fileprivate var errorText: String {
+        switch self {
+        case .invalidMailAddress:
+            return "有効でないメールアドレスです。"
+        case .differentConfirmPassword:
+            return "パスワードが一致していません。"
+        case .emptyField:
+            return "未入力の項目があります。"
+        case .other:
+            return "不明のエラーです。"
+        }
+    }
+}
